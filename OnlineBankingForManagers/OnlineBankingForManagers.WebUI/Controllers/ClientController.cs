4c7484432f237ee2cdd6174601f08c6e70d9cb9f
TimHanes <timofey.lutsenko@gmail.com>
2015-09-03 20:48:49 +0300

    Added "The database is empty"

--- OnlineBankingForManagers/OnlineBankingForManagers.WebUI/Controllers/ClientController.cs
+++ OnlineBankingForManagers/OnlineBankingForManagers.WebUI/Controllers/ClientController.cs
@@ -12,10 +12,7 @@ namespace OnlineBankingForManagers.WebUI.Controllers
     [Authorize]
     public class ClientController : Controller
     {
-        //
-        // GET: /Client/
         private IClientRepository repository;
-
 
         public int PageSize = 4;
 
@@ -25,36 +22,28 @@ namespace OnlineBankingForManagers.WebUI.Controllers
         }
         public ViewResult List(string status, string sort = "ClientId", int page = 1)
         {
-            ClientsListViewModel model = new ClientsListViewModel
-            {
-                
-                PagingInfo = new PagingInfo
+            ClientsListViewModel model = new ClientsListViewModel();
+         
+            model.PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
                     TotalItems = status == null ?
         repository.Clients.Count() :
         repository.Clients.Where(e => e.Status == status).Count()
-                },
-                CurrentStatus = status,
-                CurrentSort = sort
-            };
+                };
+                model.CurrentStatus = status;
+                model.CurrentSort = sort;
 
             model.Clients = repository.Clients
                 .Where(c => ((status == null) || (c.Status == status)))
                 .OrderBy(sort)
                 .Skip((page - 1)*PageSize)
                 .Take(PageSize);
-           // model.Clients = repository.Clients.OrderBy(sort);
-
 
             return View(model);
         }
-
-
-
-
-      
+    
         public ViewResult Edit(int clientId)
         {
             Client client = repository.Clients
