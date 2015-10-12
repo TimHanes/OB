e253eb2881f9031ff48cf26f677ba14ecd1b9504
TimHanes <timofey.lutsenko@gmail.com>
2015-10-12 21:46:11 +0300

    Added script for move login's form to centre.

---
+++ OnlineBankingForManagers/OnlineBankingForManagers.Domian/Models/ConnectionData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBankingForManagers.Domain.Models
+{
+    class ConnectionData : ConfigurationSection
+    {
+        [ConfigurationProperty("connectionString", DefaultValue = "sadfasdf", IsRequired = false)]
+        public string ConnectionString
+        {
+            get
+            {
+                return (string)this["connectionString"];
+            }
+        }
+    }
+}
